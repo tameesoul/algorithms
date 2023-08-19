e9f18a812451020e6a193fe29a356b6bfe4265d7
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-08-19 10:35:56 +0300

    parallelogram_area

---
+++ c++/parallelogram_area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std; 
+void parallelogram_area( float b , float h)
+{
+    double A = b * h;
+    cout<< A <<"\n";   
+}
+
+int main()
+{
+    parallelogram_area(5,6);
+}
