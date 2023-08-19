c2f1bf6a93b007b54e87fb189cf296ab142a9ce0
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-08-19 09:41:56 +0300

    parralleglogram_area

---
+++ c/parallelogram_area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+// parrallelogram area algorithm
+
+void parallelogram_area(int b , int h)
+{
+    int A = b * h;
+    printf("%d\n", A);
+}
+
+int main(void)
+{
+    parallelogram_area(4,5);
+}
