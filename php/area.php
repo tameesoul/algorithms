8cac8fdd2cdf67d464c32f918bfdf886e0141ab5
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-08-19 08:33:00 +0300

    calculate area of parallelogram

--- php/area.php
+++ php/area.php
@@ -4,8 +4,6 @@ function area($r)
     $A = pi()*($r*$r);
     return $A;
 };
-
-
  echo area(4);
  echo "<br>";
  echo area(6);
---
+++ php/parallelogram.php
@@ -0,0 +1,9 @@
+<?php 
+
+function parallelogram($b,$h)
+{
+    $A = $b * $h;
+    echo $A;
+}
+
+parallelogram(4,5);
