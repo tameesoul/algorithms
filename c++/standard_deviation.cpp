4036e1c2fa935722c658ca469b95fc2a9cc859e0
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-09-26 16:42:35 +0300

    Standard Deviation Calculator

---
+++ c++/standard_deviation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float standard_deviation(int n, int size[]) {
+    float sum = 0, variance = 0, avg = 0, stdev = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += size[i];
+    }
+    avg = sum / n;
+
+    for (int i = 0; i < n; i++) {
+        variance += pow(size[i] - avg, 2);
+    }
+    variance /= n;
+
+    stdev = sqrt(variance);
+
+    return stdev;
+}
+
+int main() {
+    int n; 
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int size[n];
+   
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> size[i];
+    } 
+
+    float result = standard_deviation(n, size);
+
+    cout << "The standard deviation is: " << result << "\n";
+
+    return 0;
+}
